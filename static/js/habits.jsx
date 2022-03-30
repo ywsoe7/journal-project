e3cf6ba3662bfd843ca2310d403716c7d1d63328
Yoon Soe <yoonsoe25@gmail.com>
2022-03-30 16:52:37 +0000

    habits for react

--- static/js/habits.jsx
+++ static/js/habits.jsx
@@ -1,9 +1,35 @@
-function Hello() {
-    return (
-      <ul>
-        <li>Hi World!</li>
-      </ul>
-    );
-  }
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+const { Component } = React;
+const { render } = ReactDOM;
+const styled = window.styled;
+// const { GridContainer, NavContainer, MainContainer } = styled;
 
-ReactDOM.render(<Hello />, document.querySelector('#root'));
+const GridContainer = styled.div`
+  display: grid;
+  min-height: 100%;
+  grid-template-areas:
+    "nav"
+    "main";
+  grid-template-columns: 30% 70%;
+  grid-template-rows: 1fr;
+`;
+const NavContainer = styled.div`
+  grid-area: "nav";
+`;
+const MainContainer = styled.div`
+  grid-area: "main";
+`;
+
+const MyHabits = () => {
+  return (
+    <React.Fragment>
+      <GridContainer>
+        <NavContainer>Nav</NavContainer>
+        <MainContainer>Main</MainContainer>
+      </GridContainer>
+    </React.Fragment>
+  );
+}
+
+ReactDOM.render(<MyHabits />, document.querySelector('.container'));
