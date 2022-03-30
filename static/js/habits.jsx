1c4227dc5d5d31c4ca9eee4a5e5c7e9f42f1d12f
Yoon Soe <yoonsoe25@gmail.com>
2022-03-30 19:29:54 +0000

    css styles in react

--- static/js/habits.jsx
+++ static/js/habits.jsx
@@ -7,7 +7,7 @@ const styled = window.styled;
 
 const GridContainer = styled.div`
   display: grid;
-  min-height: 100%;
+  min-height: 100vh;
   grid-template-areas:
     "nav"
     "main";
@@ -16,19 +16,49 @@ const GridContainer = styled.div`
 `;
 const NavContainer = styled.div`
   grid-area: "nav";
+  padding-top: 10px;
+  padding-left: 100px;
+  background-color: #fafad2;
 `;
 const MainContainer = styled.div`
   grid-area: "main";
+  padding-top: 10px;
+  padding-right: 100px;
 `;
+const Title = styled.div`
+  font-size: 30px;
+  margin-bottom: 100px;
+`;
+const HeaderContainer = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+`;
+const HeaderItem = styled.div`
+  padding-right: 50px;
+  : last-child {
+    padding: 0;
+  }
+  font-size: 22px;
+`;
+ 
 
 const MyHabits = () => {
   return (
-    <React.Fragment>
       <GridContainer>
-        <NavContainer>Nav</NavContainer>
-        <MainContainer>Main</MainContainer>
+        <NavContainer>
+          <Title>
+            <Goals />
+          </Title>
+        </NavContainer>
+        <MainContainer>
+          <HeaderContainer>
+            <HeaderItem>My Habits</HeaderItem>
+            <HeaderItem>History</HeaderItem>
+            <HeaderItem>Hi Yoon</HeaderItem>
+          </HeaderContainer>
+        </MainContainer>
       </GridContainer>
-    </React.Fragment>
   );
 }
 
